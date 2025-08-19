bdd8d491fa88eb52b5dec70e871090de256a2e5d
Himanshu Panwar <hpanwar875@gmail.com>
2025-08-19 21:31:03 +0530

    run the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,2 +1,23 @@
-var helloWorld = "Hello world";
-console.log(helloWorld);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var app = (0, express_1.default)();
+var cookie_parser_1 = require("cookie-parser");
+var port = 7777;
+var cors_1 = require("cors");
+var database_js_1 = require("./config/database.js");
+app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+(0, database_js_1.default)()
+    .then(function () {
+    console.log("Database connected");
+    app.listen(port, function () {
+        console.log("server is running at ".concat(port));
+    });
+}).catch(function (err) {
+    console.error("Database connection failed:", err);
+});
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,24 @@
-console.log("Hello")
+import express from "express"
+const app = express()
+import cookieParser from "cookie-parser"
+const port = 7777
+import cors from 'cors'
+import dbConnection from './config/database.js'
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+
+dbConnection()
+.then(() => {
+    console.log("Database connected")
+    app.listen(port,()=>{
+       console.log( `server is running at ${port}`)
+    })
+}).catch((err:string) => {
+    console.error("Database connection failed:", err);
+});
