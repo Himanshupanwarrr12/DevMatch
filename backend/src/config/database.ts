d9187f205beb52bf3763d6dc2154ea7a68d2c962
Himanshu Panwar <hpanwar875@gmail.com>
2025-08-20 23:53:03 +0530

    clean the code

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
-const dbConnection = async () : Promise<void> => {
-  mongoose.connect(
-    "mongodb+srv://himanshudoes:Himanshu12@cluster0.cjcbcbp.mongodb.net/DevMatch?retryWrites=true&w=majority&appName=Cluster0"
-  );
+const dbConnection = async (): Promise<void> => {
+  try {
+    await mongoose.connect(
+      process.env.MONGODB_URI || ""    );
+      
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database connection error:", error);
+    throw error;
+  }
 };
 
-export default dbConnection
+export default dbConnection;
