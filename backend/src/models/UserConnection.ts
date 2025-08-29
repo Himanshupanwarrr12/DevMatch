885cbaccdf592bbfcb6d98e9f6f26845362f3245
Himanshu Panwar <hpanwar875@gmail.com>
2025-08-29 20:00:19 +0530

    feat : UserConnection schema

---
+++ backend/src/models/UserConnection.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema, model, Document, mongo } from "mongoose";
+
+export interface UserConnection extends Document {
+    fromUserId:mongoose.Schema.Types.ObjectId
+    toUserId : mongoose.Schema.Types.ObjectId
+    status:string
+    createdAt:Date
+    updatedAt:Date
+}
+
+const UserConnectionScehma  : Schema<UserConnection> = new Schema({
+    fromUserId:{
+        type:mongoose.Schema.ObjectId,
+        required:true
+    },
+    toUserId:{
+        type:mongoose.Schema.ObjectId,
+        required:true
+    },
+    status:{
+        type:String,
+        required:true,
+        enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: `{VALUE} is incorrect status type`,
+      },
+    }
+},{
+    timestamps:true
+}
+
+)
+
+export default model<UserConnection>("userConnectionSchema",UserConnectionScehma)
