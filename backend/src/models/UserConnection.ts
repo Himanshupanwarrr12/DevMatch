250baad8750d0e9484be67190e7e7b13e18efaeb
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-04 22:40:38 +0530

    feat : UserConnection model

--- backend/src/models/UserConnection.ts
+++ backend/src/models/UserConnection.ts
@@ -1,36 +1,42 @@
-import mongoose, { Schema, model, Document, mongo } from "mongoose";
+import mongoose, { Schema, model, Document } from "mongoose";
 
 export interface UserConnection extends Document {
-    fromUserId:mongoose.Schema.Types.ObjectId
-    toUserId : mongoose.Schema.Types.ObjectId
-    status:string
-    createdAt:Date
-    updatedAt:Date
+  fromUserId: mongoose.Schema.Types.ObjectId;
+  toUserId: mongoose.Schema.Types.ObjectId;
+  status: string;
+  ConnectionDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const UserConnectionScehma  : Schema<UserConnection> = new Schema({
-    fromUserId:{
-        type:mongoose.Schema.ObjectId,
-        required:true,
-        ref:"User"
+const UserConnectionSchema: Schema<UserConnection> = new Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
-    toUserId:{
-        type:mongoose.Schema.ObjectId,
-        required:true,
-        ref:"User"
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
-    status:{
-        type:String,
-        required:true,
-        enum: {
+    status: {
+      type: String,
+      required: true,
+      enum: {
         values: ["ignored", "interested", "accepted", "rejected"],
         message: `{VALUE} is incorrect status type`,
       },
-    }
-},{
-    timestamps:true
-}
+    },
+    ConnectionDate: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-)
-
-export default model<UserConnection>("userConnectionSchema",UserConnectionScehma)
+export default model<UserConnection>("userConnection", UserConnectionSchema);
