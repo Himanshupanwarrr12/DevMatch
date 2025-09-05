b89a444a7ec59bc6926ab14c3cf38e0703f2a327
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-05 21:36:31 +0530

    feat(API) : Send request and Review request

--- backend/src/routes/request.ts
+++ backend/src/routes/request.ts
@@ -5,7 +5,6 @@ import type { Request, Response } from "express";
 import User, { type IUser } from "../models/User";
 import UserconnectionModel from "../models/UserConnection";
 
-
 interface sendConnectionRequest extends Request {
   user?: IUser;
 }
@@ -29,7 +28,12 @@ requestRouter.post(
       // check the user is present in db or not
       const user = await User.findById(toUserId);
       if (!user) {
-        return res.status(400).send("user not found!");
+        return res.status(400).json({ message: "User not found" });
+      }
+
+       // user can't send request to himself
+      if (fromUserId === toUserId) {
+        return res.status(400).send("You cannot send request to yourself");
       }
 
       // check user can't send request to toUser if toUser already sent request to fromUser and can't send connection again
@@ -46,10 +50,6 @@ requestRouter.post(
           .json({ message: "Connection request already exists " });
       }
 
-      // user can't send request to himself
-      if (fromUserId === toUserId) {
-        return res.status(400).send("You cannot send request to yourself");
-      }
 
       const connectionReq = new UserconnectionModel({
         fromUserId,
@@ -68,11 +68,43 @@ requestRouter.post(
 );
 
 requestRouter.post(
-  "request/review/:status/:requestId",
+  "/request/review/:status/:connectionId",
   userAuth,
-  (req: sendConnectionRequest, res: Response) => {
+  async (req: sendConnectionRequest, res: Response) => {
+    //take loggedInUser detail
+    const loggedInuser = req.user;
+    const { status, connectionId } = req.params;
 
-     //1.take loggedInuser and main ingredeints through url 2.declare allowed status and validate the status 3.
-    
+    //check status must be valid
+    const allowedStatus = ["accepted", "rejected"];
+    if (!status || !allowedStatus.includes(status)) {
+      return res.status(400).json({ message: "Invalid status" });
+    }
+
+    // find connection request
+    const connectionRequest = await UserconnectionModel.findOne({
+      _id: connectionId,
+      toUserId: loggedInuser!._id,
+      status: "interested",
+    });
+
+    // if connection not found
+    if (!connectionRequest) {
+      return res.status(400).json({
+        message: "Connection request not found",
+      });
+    }
+
+    // set status to updated status
+    connectionRequest.status = status;
+
+    // save the data
+    const data = await connectionRequest.save();
+
+    res
+      .status(200)
+      .json({ message: "Connection request has been processed.", data });
   }
 );
+
+export default requestRouter;
