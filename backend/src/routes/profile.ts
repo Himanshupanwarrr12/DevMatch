50e1e4eb07c1af13c920f04e032cd6c0f6c4e6c9
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-12 21:00:21 +0530

    feat : update user data in profile/view  API & update the error handle in profile/edit API

--- backend/src/routes/profile.ts
+++ backend/src/routes/profile.ts
@@ -5,19 +5,40 @@ import type { Request, Response } from "express";
 import type { IUser } from "../models/User";
 import { validateProfileEditData } from "../utils/validation";
 
-interface profileViewRequest extends Request {
+interface ProfileViewRequest extends Request {
   user?: IUser;
 }
 
 profileRouter.get(
   "/profile/view",
   userAuth,
-  async (req: profileViewRequest, res: Response) => {
+  async (req: ProfileViewRequest, res: Response) => {
     try {
       const user = req.user;
-      res.send(user);
+      // console.log(user)
+
+      const safeUser = {
+        _id:user?._id,
+        firstName:user?.firstName,
+        lastName:user?.lastName,
+        gender:user?.gender,
+        skills:user?.skills,
+        about:user?.about,
+        photoUrl:user?.photoUrl,
+        createdAt:user?.createdAt,
+        updatedAt:user?.updatedAt
+      }
+      
+      res.status(200).json({
+        success:true,
+        safeUser
+      })
     } catch (error: unknown) {
-      res.status(400).send("Error : " + error);
+      res.status(400).json({
+        success:false,
+        message: " Error fetching profile",
+        error: error instanceof Error ? error.message : "Unknown error"
+      })
     }
   }
 );
@@ -25,15 +46,15 @@ profileRouter.get(
 profileRouter.patch(
   "/profile/edit",
   userAuth,
-  async (req: profileViewRequest, res: Response) => {
+  async (req: ProfileViewRequest, res: Response) => {
     try {
+      
       //first i will validate the req
       if (!validateProfileEditData(req)) {
         throw new Error("Invalid edit request!!");
       }
       // second i will get the loggedInUser
       const loggedInUser = req.user;
-
       if (!loggedInUser) {
         return res.status(400).send("User not authenticated");
       }
@@ -47,7 +68,11 @@ profileRouter.patch(
       await loggedInUser.save();
       res.status(200).send("Edited sucessfully");
     } catch (error) {
-      res.status(400).send("Error : " + error);
+      res.status(400).json({
+        success:false,
+        message:"Error during  profile edit",
+        error: error instanceof Error ? error.message : "unknown error"
+      })
     }
   }
 );
