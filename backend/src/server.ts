725f99ea39dea2cad5fe93bcd8373aeeb338884f
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-12 21:10:33 +0530

    feat : update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,41 +1,40 @@
-import express from "express"
-const app = express()
-import cookieParser from "cookie-parser"
-const port = 7777
-import cors from 'cors'
-import dbConnection from "./config/database"
-import dotenv from "dotenv"
+import express from "express";
+const app = express();
+import cookieParser from "cookie-parser";
+const port = 7777;
+import cors from "cors";
+import dbConnection from "./config/database";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-dotenv.config()
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.json())
-app.use(cookieParser())
-
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // api routes
-import authRouter from './routes/auth'
-import userRouter from './routes/user'
-import profileRouter from "./routes/profile"
-import requestRouter from "./routes/request"
+import authRouter from "./routes/auth";
+import userRouter from "./routes/user";
+import profileRouter from "./routes/profile";
+import requestRouter from "./routes/request";
 
-
-
-app.use("/",authRouter)
-app.use("/",userRouter)
-app.use("/",profileRouter)
-app.use("/",requestRouter)
+app.use("/", authRouter);
+app.use("/", userRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 dbConnection()
-.then(() => {
-    app.listen(process.env.PORT, ()=>{
-       console.log( `server is running at ${port}`)
-    })
-}).catch((err:string) => {
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`server is running at ${port}`);
+    });
+  })
+  .catch((err: string) => {
     console.error("Database connection failed:", err);
-});
+  });
