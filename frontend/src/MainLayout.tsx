14a58880692fbbd9339e214ca59cbfc8e17f8a35
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-13 23:03:53 +0530

    feat: create MainLayout with base structure (header, footer, content area)

---
+++ frontend/src/MainLayout.tsx
@@ -0,0 +1,18 @@
+
+import { Outlet } from "react-router-dom"
+import Footer from "./ui/footer/Footer"
+import Navbar from "./ui/header/Navbar"
+
+const MainLayout = () => {
+  return (
+    <div>
+        <Navbar/>
+        <main>
+            <Outlet/>
+        </main>
+        <Footer/>
+    </div>
+  )
+}
+
+export default MainLayout
