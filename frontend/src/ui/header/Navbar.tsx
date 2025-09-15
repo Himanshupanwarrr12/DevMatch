fe8e90b56bf471a6181acf5bbf54dbab9a1c633e
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-15 23:33:48 +0530

    feat : improve the ui

--- frontend/src/ui/header/Navbar.tsx
+++ frontend/src/ui/header/Navbar.tsx
@@ -3,15 +3,17 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div className="h-[80px] bg-violet-600 w-full p-2 flex justify-between ">
+      <div className="h-[60px] bg-stone-500 w-full p-2 flex justify-between items-center ">
         <div className="">
-          <NavLink to="/" className="text-xl text-black ">DevMatch</NavLink>
+          <NavLink to="/" className="text-xl items-center  text-white ">DevMatch</NavLink>
         </div>
-        <div>
-          Profile
+        <div className="flex justify-center items-center gap-4 text-xl" >
+          <h1 className="text-white" >
+            Profile
+          </h1>
           <div>
             <img 
-            className="h-[16px] w-[16px]"
+            className="h-[32px] w-[32px]"
              src="https://www.shutterstock.com/shutterstock/photos/1677509740/display_1500/stock-vector-default-avatar-profile-icon-social-media-user-vector-1677509740.jpg" alt="user pic" />
           </div>
         </div>
