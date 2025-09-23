57d41c8f7a2247c4eeabd5a18ddface3559ca0a9
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-24 03:46:49 +0530

    feat : rtk store

---
+++ frontend/src/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from '../features/user/userSlice'
+
+const store = configureStore({
+  reducer: {
+    user:userReducer
+  },
+});
+
+export type RootState =  ReturnType<typeof store.getState>
+export type Dispatch = typeof store.dispatch
+
+export default store;   
