6383e061f944c97891c9270e2f67a68fb7873d61
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-24 23:04:48 +0530

    feat : update ui

--- frontend/src/ui/header/Navbar.tsx
+++ frontend/src/ui/header/Navbar.tsx
@@ -2,25 +2,21 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <>
-      <div className="h-[60px] bg-stone-500 w-full p-2 flex justify-between items-center ">
-        <div className="">
-          <NavLink to="/" className="text-xl items-center  text-white ">DevMatch</NavLink>
-        </div>
-        <div className="flex justify-center items-center gap-4 text-xl" >
-          <h1 className="text-white" >
-            Profile
-          </h1>
-          <div>
-            <img 
-            className="h-[32px] w-[32px]"
-             src="https://www.shutterstock.com/shutterstock/photos/1677509740/display_1500/stock-vector-default-avatar-profile-icon-social-media-user-vector-1677509740.jpg" alt="user pic" />
-          </div>
-        </div>
+    <div className="h-[60px] bg-pink-600 w-full p-2 flex justify-between items-center">
+      <NavLink to="/" className="text-xl text-white">
+        DevMatch
+      </NavLink>
+      
+      <div className="flex items-center gap-4 text-xl">
+        <span className="text-white">Profile</span>
+        <img 
+          className="h-8 w-8 rounded-full"
+          src="https://www.shutterstock.com/shutterstock/photos/1677509740/display_1500/stock-vector-default-avatar-profile-icon-social-media-user-vector-1677509740.jpg" 
+          alt="User profile picture" 
+        />
       </div>
-    </>
+    </div>
   );
 };
 
 export default Navbar;
-
