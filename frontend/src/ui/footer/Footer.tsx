bced6ed16b0727f770ea6d1b57e44dcdcef7c26b
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-24 23:04:29 +0530

    feat :  made whole footer section

--- frontend/src/ui/footer/Footer.tsx
+++ frontend/src/ui/footer/Footer.tsx
@@ -1,8 +1,11 @@
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 bg-lime-300 w-full h-[50px]">
-      Footer</div>
+    <footer className="footer sm:footer-horizontal footer-center bg-stone-600 text-base-content p-4">
+  <aside>
+    <p className="text-white text-lg">Copyright © {new Date().getFullYear()} - All right reserved by DevMatch</p>
+  </aside>
+</footer>
   )
 }
 
