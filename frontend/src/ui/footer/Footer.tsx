5b397fc8b5fa7e51740d928f2f994fed5aab3e17
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-25 22:28:10 +0530

    feat : change color

--- frontend/src/ui/footer/Footer.tsx
+++ frontend/src/ui/footer/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal footer-center bg-stone-600 text-base-content p-4">
+    <footer className="footer sm:footer-horizontal footer-center bg-pink-600 text-base-content p-4">
   <aside>
     <p className="text-white text-lg">Copyright © {new Date().getFullYear()} - All right reserved by DevMatch</p>
   </aside>
