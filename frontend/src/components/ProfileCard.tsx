8884ccde70b36af46e7b57b3cce451007ac1d1ca
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-25 22:25:47 +0530

    feat : basic setup for profile card

---
+++ frontend/src/components/ProfileCard.tsx
@@ -0,0 +1,19 @@
+
+export const ProfileCard = () => {
+  return (
+    <div>
+        <div>
+            <img src="" alt="user img" />
+        </div>
+        <div>
+            <h3>name</h3>
+            <p>skills</p>
+            <p>about</p>
+            <p>future interest</p>
+        </div>
+        <div>
+            links
+        </div>
+    </div>
+  )
+}
