f8f87402136f9fc8ad41b5f831c2bfb89d0dca6a
Himanshu Panwar <hpanwar875@gmail.com>
2025-09-29 22:02:52 +0530

    feat : made dynamic name of user

--- frontend/src/ui/header/Navbar.tsx
+++ frontend/src/ui/header/Navbar.tsx
@@ -1,6 +1,10 @@
-import { NavLink } from "react-router-dom";
+import type { RootState } from "../../store/store";
+import { useSelector } from "react-redux";
+import {  NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const  user = useSelector((store : RootState)=>store.user.user)
+  // console.log("user : ",user)
   return (
     <div className="h-[60px] bg-pink-600 w-full p-2 flex justify-between items-center">
       <NavLink to="/" className="text-xl text-white">
@@ -8,7 +12,7 @@ const Navbar = () => {
       </NavLink>
       
       <div className="flex items-center gap-4 text-xl">
-        <span className="text-white">Profile</span>
+        <h3>{user?.firstName}</h3>
         <img 
           className="h-8 w-8 rounded-full"
           src="https://www.shutterstock.com/shutterstock/photos/1677509740/display_1500/stock-vector-default-avatar-profile-icon-social-media-user-vector-1677509740.jpg" 
