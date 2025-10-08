9b20b7266c12cca274ec550140f4ee5be9443663
Himanshu Panwar <hpanwar875@gmail.com>
2025-10-08 23:33:17 +0530

    feat(working) : prevent user from disappearing on page reloads

--- frontend/src/MainLayout.tsx
+++ frontend/src/MainLayout.tsx
@@ -2,8 +2,27 @@
 import { Outlet } from "react-router-dom"
 import Footer from "./ui/footer/Footer"
 import Navbar from "./ui/header/Navbar"
+import { useEffect } from "react"
+import axios from "axios"
+import { baseUrl } from "./utils/constant"
 
 const MainLayout = () => {
+
+  // 
+
+  const fetchUser = async ()=>{
+
+    try {
+      const res = await axios.get(baseUrl + "")
+    } catch (error:unknown) {
+      console.log("Error : ",error)
+    }
+  }
+  
+  useEffect(()=>{
+    fetchUser()
+  },[])
+  
   return (
     <div >
         <Navbar/>
