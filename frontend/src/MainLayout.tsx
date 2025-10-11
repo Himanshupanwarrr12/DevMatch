6bd7f631ff0d609e2e663b1138e3b8a86489cfcd
Himanshu Panwar <hpanwar875@gmail.com>
2025-10-11 23:45:32 +0530

    feat : no logout after reload

--- frontend/src/MainLayout.tsx
+++ frontend/src/MainLayout.tsx
@@ -1,24 +1,40 @@
 
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import Footer from "./ui/footer/Footer"
 import Navbar from "./ui/header/Navbar"
 import { useEffect } from "react"
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 import { baseUrl } from "./utils/constant"
+import { useDispatch } from "react-redux"
+import { addUser } from "./features/user/userSlice"
 
 const MainLayout = () => {
 
   // 
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const fetchUser = async ()=>{
-
-    try {
-      const res = await axios.get(baseUrl + "")
-    } catch (error:unknown) {
-      console.log("Error : ",error)
-    }
-  }
   
+  const fetchUser = async ()=>{
+    try {
+      const res = await axios.get(baseUrl + "/profile/view",{
+        withCredentials:true
+      })
+      dispatch(addUser(res.data))
+
+      
+    } catch (error) {
+
+       if (axios.isAxiosError(error)) {
+        const AxiosError = error as AxiosError;
+
+        if(AxiosError.response?.status === 401 ){
+          navigate("/login")
+        }
+       
+      console.log("Error : ",error)
+  }}
+}
   useEffect(()=>{
     fetchUser()
   },[])
