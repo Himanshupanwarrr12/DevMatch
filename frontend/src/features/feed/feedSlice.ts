071970c34ccc74f218301565762b4079b5d63409
Himanshu Panwar <hpanwar875@gmail.com>
2025-10-28 22:07:03 +0530

    feat: imrpoved code of feed slice

--- frontend/src/features/feed/feedSlice.ts
+++ frontend/src/features/feed/feedSlice.ts
@@ -1,24 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-export interface feed {
+export interface Feed {
+  _id : string;
   firstName: string;
-  lastName: string;
-  email: string;
   gender: string;
+  about:string;
+  sklls:string[]
+  photoUrl:string;
 }
 
-const initialState = null as feed | null
+const initialState:  Feed[] = []
 
 export const feedSlice = createSlice({
-    name:"feed",
-    initialState,
-    reducers:{
-        showFeed : ((state,action: PayloadAction<feed>)=>{
-            return action.payload
-        }),
-        removeUserFromFeed : ((state,action)=>{
+  name: "feed",
+  initialState,
+  reducers: {
+    setFeed: (state, action: PayloadAction<Feed[]>) => {
+      return action.payload;
+    },
+    removeUserFromFeed: (state, action: PayloadAction<string>) => {
+      const newFeed = state.filter((user) => user._id !== action.payload);
+      return newFeed;
+    },
+  },
+});
 
-        })
-    }
-})
+export const { setFeed,removeUserFromFeed }  = feedSlice.actions
+export default feedSlice.reducer
