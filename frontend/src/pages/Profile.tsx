5534dafa7892fb7895e175f66851a52c0926aaf4
Himanshu Panwar <hpanwar875@gmail.com>
2025-10-28 22:09:17 +0530

    feat: added a card

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,12 +1,15 @@
 import type { RootState } from "../store/store";
 import { useSelector } from "react-redux";
+import EditProfileCard from "@/components/EditProfileCard";
 
 const Profile = () => {
   const user = useSelector((store: RootState) => {
     return store.user;
   });
 
-  return <div>cooking...</div>;
+  if(!user) return <div className="text-center p-20 font-bold text-red-600 text-3xl">Loading Profile...</div>
+
+  return < EditProfileCard user={user}/>
 };
 
 export default Profile;
