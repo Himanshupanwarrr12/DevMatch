a2fd0fc4fdc3651cf42e3be935db7a4337763c59
Himanshu Panwar <hpanwar875@gmail.com>
2025-10-28 22:05:39 +0530

    feat: made profileCard dynamic

--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -1,23 +1,32 @@
-const ProfileCard = () => {
+import type { User } from "@/features/user/userSlice";
+
+interface profileCardProps {
+  user:User
+}
+
+const ProfileCard = ({user}:profileCardProps) => {
+
+  const {photoUrl,firstName,skills,about,futureInterests} = user
+  
   return (
     <div className="  bg-white p-4 pl-5 pr-5 w-[350px] shadow-2xl ">
       <div className=" flex justify-center ">
         <img
           alt=" img"
-          src="https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg"
+          src={photoUrl}
           className=" border-4 object-cover p-2 m-2 rounded-full h-40 w-40  border-gray-300"
         />
       </div>
 
       <div className="p-3 ">
         <h3 className="text-center text-rose-700 pb-2 font-bold text-2xl">
-          Himanshu{" "}
+          {firstName}{" "}
         </h3>
         <p className="text-center font-semibold p-1 m-1 text-gray-600 font-serif">
-          React.Js | Typescript | Next.js
+          {skills}
         </p>
-        <p className="text-center ">about</p>
-        <p className="text-center">future interest</p>
+        <p className="text-center ">{about}</p>
+        <p className="text-center">{futureInterests}</p>
       </div>
 
       <div className="mt-2 text-blue-800 font-semibold">
