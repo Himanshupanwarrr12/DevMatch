5b1e64e5ef8873d5f04ae82b4635ff039c34fe2f
Himanshu Panwar <hpanwar875@gmail.com>
2025-10-30 22:52:59 +0530

    added lastName links gender to the profile card

--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -1,35 +1,47 @@
 import type { User } from "@/features/user/userSlice";
 
 interface ProfileCardProps {
-  user: Partial<User>; // Only this change for type safety
+  user: Partial<User>; 
 }
 
 const ProfileCard = ({ user }: ProfileCardProps) => {
-  const { photoUrl, firstName, skills, about, futureInterests } = user;
-  
+  const {
+    firstName,
+    lastName,
+    about,
+    gender,
+    photoUrl,
+    skills,
+    futureInterest,
+    links,
+  } = user;
+
+  console.log("Links" ,links)
+
   return (
     <div className="bg-white p-4 pl-5 pr-5 w-full max-w-[350px] mx-auto shadow-2xl rounded-2xl">
       <div className="flex justify-center">
         <img
           alt="img"
           src={photoUrl}
-          className="border-4 object-cover p-2 m-2 rounded-full h-40 w-40 border-rose-300"
+          className=" object-cover p-2 m-2 rounded-full h-40 w-40 "
         />
       </div>
 
       <div className="p-3">
-        <h3 className="text-center text-rose-700 pb-2 font-bold text-2xl">
-          {firstName}
+        <h3 className="text-center text-rose-700  font-bold text-xl">
+          {firstName} {lastName}
         </h3>
+        <p className="text-center">{gender}</p>
         <p className="text-center font-semibold p-1 m-1 text-gray-600 font-serif">
           {skills}
         </p>
         <p className="text-center">{about}</p>
-        <p className="text-center">{futureInterests}</p>
+        <p className="text-center">{futureInterest}</p>
       </div>
 
       <div className="mt-2 text-blue-800 font-semibold">
-        <a href="https://github.com">Github</a>
+        <a href="https://github.com">{links}</a>
       </div>
     </div>
   );
