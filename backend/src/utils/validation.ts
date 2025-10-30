fe1f62955c99928a14e577981ea4e448737a6c6b
Himanshu Panwar <hpanwar875@gmail.com>
2025-10-30 22:50:39 +0530

    added more fields to edit

--- backend/src/utils/validation.ts
+++ backend/src/utils/validation.ts
@@ -1,25 +1,38 @@
-import validator from 'validator'
-import type { Request } from 'express'
+import validator from "validator";
+import type { Request } from "express";
 
-export  function validateSignUpData(req:Request){
-    const {firstName,emailId,password} = req.body
-    // console.log("req.body : ",req.body)
-    if(!firstName || typeof firstName !== 'string' || firstName.trim().length === 0){
-        throw new Error("First Name is required and must be a  non-empty string.")
-    }
-    if(!emailId || !validator.isEmail(emailId)){
-        throw new Error("valid email is required ")
-    }
-    if( !password || !validator.isStrongPassword(password)){
-        throw new Error("Password must be strong.")
-    }
-
+export function validateSignUpData(req: Request) {
+  const { firstName, emailId, password } = req.body;
+  // console.log("req.body : ",req.body)
+  if (
+    !firstName ||
+    typeof firstName !== "string" ||
+    firstName.trim().length === 0
+  ) {
+    throw new Error("First Name is required and must be a  non-empty string.");
+  }
+  if (!emailId || !validator.isEmail(emailId)) {
+    throw new Error("valid email is required ");
+  }
+  if (!password || !validator.isStrongPassword(password)) {
+    throw new Error("Password must be strong.");
+  }
 }
 
-export  function validateProfileEditData(req:Request){
-    const editFieldsAllowed = ["firstName","lastName","emailId","photoUrl","about","skills"]
-    const isEditAllowed = Object.keys(req.body).every((field)=>{
-      return  editFieldsAllowed.includes(field)
-    })
-    return isEditAllowed
+export function validateProfileEditData(req: Request) {
+  const editFieldsAllowed = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "about",
+    "gender",
+    "skills",
+    "futureInterest",
+    "links",
+  ];
+  const isEditAllowed = Object.keys(req.body).every((field) => {
+    return editFieldsAllowed.includes(field);
+  });
+  return isEditAllowed;
 }
