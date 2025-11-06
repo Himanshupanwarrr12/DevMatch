0ca97d05a483de77387ea2442c05813194df9efd
Himanshu Panwar <hpanwar875@gmail.com>
2025-11-06 20:54:57 +0530

    added connections reducer to the store

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
 import feedReducer from "../features/feed/feedSlice"
+import connnectionsReducer from "../features/connection/connectionSlice"
 
 const store = configureStore({
   reducer: {
     user: userReducer,
-    feed: feedReducer
+    feed: feedReducer,
+    connections:connnectionsReducer
   },
 });
 
