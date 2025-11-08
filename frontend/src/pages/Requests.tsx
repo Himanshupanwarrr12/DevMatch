72510c4cfc85c933e50663a87354cc6498bd49ad
Himanshu Panwar <hpanwar875@gmail.com>
2025-11-08 23:17:17 +0530

    just checked the api and fetched the requests

--- frontend/src/pages/Requests.tsx
+++ frontend/src/pages/Requests.tsx
@@ -1,7 +1,31 @@
-const Requests = () => {
-  return (
-    <div> cooking...</div>
-  )
-}
+import axiosInstance from "@/utils/axios.config";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 
-export default Requests
+const Requests = () => {
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+
+  const fetchedRequests = async () => {
+    try {
+      const res = await axiosInstance.get("/user/requests/recieved");
+      console.log("Res of requests : ", res.data.data);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setError(error.message);
+        console.error("Feed Error:", error.message);
+      } else {
+        setError("An unexpected error occurred");
+        console.error("Unknown Error:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchedRequests();
+  }, []);
+
+  return <div> cooking...</div>;
+};
+
+export default Requests;
