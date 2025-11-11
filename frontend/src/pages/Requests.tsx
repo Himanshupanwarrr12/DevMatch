ec81ed7b7859a652d91b7c7390893bdc70b4fdf4
Himanshu Panwar <hpanwar875@gmail.com>
2025-11-11 22:11:31 +0530

    dispatched requests to the store & maded a basic div

--- frontend/src/pages/Requests.tsx
+++ frontend/src/pages/Requests.tsx
@@ -1,6 +1,10 @@
+import { setRequests } from "@/features/request/requestsSlice";
 import axiosInstance from "@/utils/axios.config";
+import type { RootState } from "../store/store";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+
+
 
 const Requests = () => {
   const [error, setError] = useState("");
@@ -10,6 +14,7 @@ const Requests = () => {
     try {
       const res = await axiosInstance.get("/user/requests/recieved");
       console.log("Res of requests : ", res.data.data);
+      dispatch(setRequests(res.data.data))
     } catch (error: unknown) {
       if (error instanceof Error) {
         setError(error.message);
@@ -25,7 +30,28 @@ const Requests = () => {
     fetchedRequests();
   }, []);
 
-  return <div> cooking...</div>;
+
+  const requests = useSelector((store:RootState)=> store.requests)
+
+  if(!requests) return null;
+  if(requests.length === 0) return <p className="text-red-600 text-2xl text-center">No requests found</p>
+
+  return (
+    <div className="flex flex-col gap-5  py-4">
+      <h1 className="text-white text-3xl text-center font-bold">Requests</h1>
+
+      { requests.map((requestObj)=>{
+        console.log(" RequestObj : ",requestObj.fromUserId)
+        
+        return(
+          <div key={requestObj._id}> HI </div>s
+        )
+        
+      }) }
+
+      
+    </div>
+  )
 };
 
 export default Requests;
