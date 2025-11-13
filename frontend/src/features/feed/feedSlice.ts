3394c045b14619eab9bf6ac015cc221e42961a46
Himanshu Panwar <hpanwar875@gmail.com>
2025-11-13 20:04:33 +0530

    feat : add addUserBackToFeed action for rollback

--- frontend/src/features/feed/feedSlice.ts
+++ frontend/src/features/feed/feedSlice.ts
@@ -15,8 +15,11 @@ export const feedSlice = createSlice({
       const newFeed = state.filter((user) => user._id !== action.payload);
       return newFeed;
     },
+    addUserBackToFeed:(state,action) =>{
+      state.unshift(action.payload)
+    }
   },
 });
 
-export const { setFeed,removeUserFromFeed }  = feedSlice.actions
+export const { setFeed,removeUserFromFeed,addUserBackToFeed }  = feedSlice.actions
 export default feedSlice.reducer
