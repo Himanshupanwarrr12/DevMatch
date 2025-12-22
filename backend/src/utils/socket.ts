e0a8f0fd9772d152b2cb763941af09f49d94d8dc
Himanshu Panwar <hpanwar875@gmail.com>
2025-12-22 06:55:57 +0530

    feat(chat): implement room-based real-time messaging using Socket.IO

--- backend/src/utils/socket.ts
+++ backend/src/utils/socket.ts
@@ -1,32 +1,31 @@
-import {Server} from "socket.io"
-import type { Server as HTTPServer} from "http"
+import { Server } from "socket.io";
+import type { Server as HTTPServer } from "http";
+import crypto from "crypto"
 
+const intializeSocket = (server: HTTPServer) => {
+  const io = new Server(server, {
+    cors: { origin: "http://localhost:5173" },
+  });
 
-const intializeSocket = (server:HTTPServer) => {
-    const io = new Server(server,{
-    cors:{origin:"http://localhost:5173"}
-})
+  io.on("connection", (socket) => {
+    socket.on("joinChat", ({ firstName, toUserId, userId }) => {
+      const roomId: string = [userId, toUserId].sort().join("_");
+      console.log(`${firstName} joined on room ${roomId}`);
+      socket.join(roomId);
+    });
 
-io.on("connection",(socket)=>{
+    socket.on(
+      "sendMessage",
+      ({ firstName, lastName, userId, toUserId, text }) => {
+        const rooomId : string = [userId,toUserId].sort().join("_")
+        io.to(rooomId).emit("messageRecieved",{firstName,lastName,text})
+      }
+    );
 
-    socket.on("joinChat",({firstName,toUserId,userId})=>{
-        const roomId:string = [userId,toUserId].sort().join("_");
-        console.log(`${firstName} joined on room ${roomId}`)
-        socket.join(roomId)
-    })
+    socket.on("disconnect", () => {
+        console.log("User disconnected")
+    });
+  });
+};
 
-    socket.on("sendMessage",()=>{
-
-    })
-
-    socket.on("disconnect",()=>{
-        
-    })
-    
-})
-
-}
-
-export default intializeSocket
-
-
+export default intializeSocket;
