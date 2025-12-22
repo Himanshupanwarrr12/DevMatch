a2d8c3fc125b030e37339b097c57f715d25d450a
Himanshu Panwar <hpanwar875@gmail.com>
2025-12-22 07:01:14 +0530

    feat : maded input working and ui dynamic

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,5 +1,5 @@
 import createsocketConnection from "@/utils/socket";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import type { RootState } from "@/store/store";
@@ -10,8 +10,27 @@ const Chat = () => {
   const {toUserId} =useParams()
   const user = useSelector((store:RootState)=> store.user) as User | null;
   const firstName = user?.firstName
+  const lastName = user?.lastName
   const userId = user?._id
 
+  const [messages,setMessages] = useState<string[]>([])
+  const [newMessage,setNewMessage] = useState("")
+
+  function sendMessage (){
+    const socket = createsocketConnection();
+
+    setMessages([...messages,newMessage])
+
+    socket?.emit("sendMessage",{
+      firstName:firstName,
+      lastName:lastName,
+      userId,
+      toUserId,
+      text:newMessage
+    })
+    setNewMessage("")
+  }
+  
   useEffect(() => {
     if(!userId) return
     const socket = createsocketConnection();
@@ -22,12 +41,16 @@ const Chat = () => {
       userId
     })
 
+    socket?.on("messageRecieved",({firstName,lastName,text})=>{
+      console.log(`${firstName} ${lastName} says ${text}`)
+    } )
+
 
     return ()=>{
       socket?.disconnect()
     }
     
-  }, [userId,firstName,toUserId]);
+  }, [userId,toUserId,firstName]);
 
   return (
     <div className="flex flex-col h-screen bg-white p-4">
@@ -35,7 +58,7 @@ const Chat = () => {
       <div className="flex-1 overflow-y-auto bg-gray-300 rounded-2xl p-4 shadow">
         <div className="mb-3 text-left">
           <div className="inline-block bg-white text-gray-800 p-2 rounded-2xl max-w-xs">
-            Hello! How can I help you?
+            {messages}
           </div>
         </div>
 
@@ -50,10 +73,12 @@ const Chat = () => {
       <div className="mt-4 flex gap-2">
         <input
           type="text"
+          value={newMessage}
+          onChange={(e)=> setNewMessage(e.target.value)}
           placeholder="Type a message..."
           className="flex-1 p-3 border border-gray-300 rounded-2xl focus:outline-none"
         />
-        <button className="px-4 py-2 bg-blue-500 text-white rounded-2xl shadow">
+        <button onClick={sendMessage} className="px-4 py-2 bg-blue-500 text-white rounded-2xl shadow">
           Send
         </button>
       </div>
