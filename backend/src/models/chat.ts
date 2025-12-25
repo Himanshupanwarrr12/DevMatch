dfeb2e3d6a2e7c2fbcedf5e5c2445eeaec7bf38f
Himanshu Panwar <hpanwar875@gmail.com>
2025-12-25 21:30:50 +0530

    feat : maded chatSchema for storing chat messages in db

--- backend/src/models/chat.ts
+++ backend/src/models/chat.ts
@@ -1,11 +1,40 @@
-import { Schema } from "mongoose"
+import mongoose, { model, Schema, Document, Types } from "mongoose"
 
+interface IMessage {
+    senderId: Types.ObjectId;
+    text: string;
+}
 
-const messageSchema = new Schema({
+interface IMessageDocument extends IMessage, Document {
+    _id: Types.ObjectId;
+}
 
-    
+interface IChat extends Document {
+    participants: Types.ObjectId[];
+    messages: IMessageDocument[];
+}
+
+const messageSchema = new Schema<IMessage>({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    text: {
+        type: String,
+        required: true,
+    },
 })
 
-const chatSchema = new Schema({
-    
+const chatSchema = new Schema<IChat>({
+    participants: [
+        { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "User", 
+            required: true 
+        },
+    ],
+    messages: [messageSchema],
 })
+
+export default model<IChat>("chat", chatSchema)
